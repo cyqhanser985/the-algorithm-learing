5e28b9de41dea3c486cb71437cd8e019ec2c7bed
cyqhanser985 <2660920607@qq.com>
2025-04-13 16:09:57 +0800

    Create 无字符串函数调用版.cpp

---
+++ 字符串/字符串/语句解析/无字符串函数调用版.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    set<char,int> s;
+    string s1;
+    cin>>s1;
+    int l = s1.length();
+    int n = l/4;
+    for(int i=0;i<n;i++){
+        char c1 = s1[4*i];
+        char c2 = s1[4*i+3];
+        if(c2>='0' && c2<='9') s[c1]=c2-'0';
+        else{
+            if(s.find(c2)!=s.end()) s[c1]=s[c2];
+            else {
+                s[c1]=0;
+                s[c2]=0;
+            }
+        }
+    }
+    cout<<s['a']<<" "<<s['b']<<" "<<s['c']<<endl;
+}
